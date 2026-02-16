77206175427e707949992932a519ee9ce9893d67
abdulkayoum1kpro <abdulkayoum1k@gmail.com>
2026-02-17 02:08:32 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+let mIconElement=document.querySelector("#mIcon")
+let navElement=document.querySelector("nav")
+mIconElement.addEventListener("click",()=>{
+    navElement.classList.toggle("left-[0px]")
+})
